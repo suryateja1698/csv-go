1ffd7ab0714b09bc45f5eb628a0c47de90a27a9a
Surya Teja Marella <suryatejam98@gmail.com>
2022-06-08 10:32:10 +0530

    simplify code

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"unicode"
 )
 
 type Players struct {
@@ -40,7 +38,6 @@ func csvParse(file *os.File) ([]Players, error) {
 	csvReader := csv.NewReader(file)
 
 	csvReader.LazyQuotes = true
-	csvReader.Comma = ';'
 	csvReader.FieldsPerRecord = -1
 	csvReader.Read()
 	data, err := csvReader.ReadAll()
@@ -51,43 +48,12 @@ func csvParse(file *os.File) ([]Players, error) {
 	playersList := make([]Players, 0, len(data)-1)
 	var rec Players
 	for _, line := range data {
-		for _, s := range line {
-			fields := strings.Split(s, ",")
-			for j, f := range fields {
-				switch j {
-				case 0:
-					sanitizedValue := Sanitize(f)
-					rec.FirstName = sanitizedValue
-				case 1:
-					sanitizedValue := Sanitize(f)
-					rec.LastName = sanitizedValue
-				case 2:
-					sanitizedValue := Sanitize(f)
-					rec.Country = sanitizedValue
-				case 3:
-					sanitizedValue := Sanitize(f)
-					rec.Role = sanitizedValue
-				}
-			}
-		}
-		playersList = append(playersList, Players{
-			FirstName: rec.FirstName,
-			LastName:  rec.LastName,
-			Country:   rec.Country,
-			Role:      rec.Role,
-		})
+		rec.FirstName = line[0]
+		rec.LastName = line[1]
+		rec.Country = line[2]
+		rec.Role = line[3]
+		playersList = append(playersList, rec)
 	}
 	return playersList, nil
 
-}
-
-// Sanitize is used to sanitize the string from special characters and unreadable characters
-func Sanitize(s string) string {
-	sanitizedString := strings.Map(func(r rune) rune {
-		if unicode.IsPrint(r) {
-			return r
-		}
-		return -1
-	}, s)
-	return sanitizedString
 }
